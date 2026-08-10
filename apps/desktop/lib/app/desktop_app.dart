1aaab92af8804accfdee5c151c780564ede96548
Ali <98952443+Aliturgutt@users.noreply.github.com>
2026-08-10 18:43:03 +0300

    DESKTOP.D07 expose governed approval callback to shell

--- apps/desktop/lib/app/desktop_app.dart
+++ apps/desktop/lib/app/desktop_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../control_plane/client.dart';
 import '../control_plane/operational_snapshot.dart';
 import '../control_plane/projection.dart';
 import '../features/dashboard/desktop_shell.dart';
@@ -11,13 +12,18 @@ class IlaiosDesktopApp extends StatelessWidget {
     this.projection = const ControlPlaneProjection.unavailable(),
     this.operationalSnapshot = const OperationalSnapshot.unavailable(),
     this.operationalStatus = 'Operational APIs not connected',
+    this.approverId,
     this.onRefreshRequested,
+    this.onGovernanceDecision,
   });
 
   final ControlPlaneProjection projection;
   final OperationalSnapshot operationalSnapshot;
   final String operationalStatus;
+  final String? approverId;
   final VoidCallback? onRefreshRequested;
+  final Future<void> Function(String requestId, GovernanceDecision decision)?
+      onGovernanceDecision;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +35,9 @@ class IlaiosDesktopApp extends StatelessWidget {
         projection: projection,
         operationalSnapshot: operationalSnapshot,
         operationalStatus: operationalStatus,
+        approverId: approverId,
         onRefreshRequested: onRefreshRequested,
+        onGovernanceDecision: onGovernanceDecision,
       ),
     );
   }
